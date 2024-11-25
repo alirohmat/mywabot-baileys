bbff4d683887c5b33643a677b235696d34a5ce38
alirohmat <71525350+alirohmat@users.noreply.github.com>
2024-11-25 10:34:34 +0700

    Update setting.js

--- setting.js
+++ setting.js
@@ -4,8 +4,8 @@ pengaturan lainnya ada di db.setting. anda bisa gunakan command owner. default b
 Other settings are in db.setting. you can use the owner command. default language is id. do the command - db.setting.lang = "en" or any other language code to change it
 */
 export default {
-    owner: ["6285157489446"],
-    typedb: "json", // use json / mongo
+    owner: ["6282132789470"],
+    typedb: "mongo", // use json / mongo
 
     /* database setting */
     db: {
